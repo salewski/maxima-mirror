df0fa10474ab2bed1f76882a6e130744c5ed2df9
Robert Dodier <robert_dodier@sourceforge.net>
2024-01-27 13:07:34 -0800

    Package abs_integrate: changes to correct behavior in presence of lexical symbols.
     * src/mlisp.lisp: new keyword argument :DECLARE-INVISIBLY for DECLARE1.
       This argument is only accessible by calling from Lisp; it can't be
       accessed via declare(...) in Maxima. :DECLARE-INVISIBLY suppresses
       putting the declared symbol on the infolist 'props'.
    
     * share/contrib/integration/abs_integrate.lisp: declare signum as real,
       invisibly; declare global variables extra_integration_methods and
       extra_definite_integration_methods as nonlexical.
    
     * share/contrib/integration/partition.mac: declare global variable %voi as nonlexical.
    
     * share/contrib/integration/rtest_abs_integrate.mac: move
       declare(signum,real) out of this file; don't kill functions and
       values at the end, so as to preserve them for investigation after the
       fact; remove forget(signum,real) as it had no effect (correct call is
       remove(signum,real)) and also we want signum to always be real.

--- share/contrib/integration/abs_integrate.lisp
+++ share/contrib/integration/abs_integrate.lisp
@@ -10,6 +10,11 @@
 
 (in-package :maxima)
 
+;; Has the same effect as declare(signum, real) in user input,
+;; but this survives kill(all); consider moving this into src.
+
+(declare1 (list '$signum) t '$real 'kind :declare-invisibly t)
+
 ;; Maxima list of additional integration methods.
 (defmvar $extra_integration_methods 
   (simplifya (list (list 'mlist) '$intfudu '$intfugudu '$signum_int 
@@ -18,6 +23,13 @@
 ;; Maxima list of additional definite integration methods.
 (defmvar $extra_definite_integration_methods 
   (simplifya (list (list 'mlist) '$abs_defint) t))
+
+;; Declare preceding user-visible special variables as nonlexical.
+;; It would be much better to roll this declaration into the DEFMVAR macro;
+;; need to circle back to that.
+
+(with-context-$global
+  (declare1 (list '$extra_integration_methods '$extra_definite_integration_methods) t '$nonlexical 'kind :declare-invisibly t))
 
 ;; Maxima interface to return the value of the special  *integrator-level*
 ;; When it's unbound, return zero.
